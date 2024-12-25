d0f0cdd1c709ce86947e251da60d540faa988c06
devwenderson <wend.devdesigner@gmail.com>
2024-12-25 14:08:30 -0300

    fix: ajusta campo user em todos

--- api/apps/todo/serializers.py
+++ api/apps/todo/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from apps.todo.models import Todo
 
 class TodoSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(source='user.name', read_only=True)
     class Meta:
         model = Todo
-        fields = '__all__'
+        fields = ['title', 'user', 'is_complete']

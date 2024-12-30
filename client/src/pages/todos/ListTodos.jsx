1fb33de258804d4edca36fbbbaa01fcd002f2cd9
devwenderson <wend.devdesigner@gmail.com>
2024-12-30 00:21:27 -0300

    feat: adiciona listagem de tarefas

--- client/src/pages/todos/ListTodos.jsx
+++ client/src/pages/todos/ListTodos.jsx
@@ -1,42 +1,61 @@
 import { useEffect, useState } from "react";
-// import ApiWrapper from "../../functions/apiWrapper"; 
-
-// const apiWrapper = new ApiWrapper()
+import TodoWrapper from "../../functions/todoWrapper"; 
+import 'bootstrap/dist/css/bootstrap.min.css'
+const todoWrapper = new TodoWrapper()
 
 const ListTodos = () => {
-    // const [todos, setTodos] = useState([]);
-    // const [isLoading, setIsLoading] = useState(false)
-    // const [error, setError] = useState(null)
+    const [todos, setTodos] = useState([]);
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
 
-    // const fetchTodos = async () => {
-    //     try {
-    //         setIsLoading(true);
-    //         const todosData = await apiWrapper.listAll('/todos/');
-    //         setTodos(todosData);
-    //     } catch (error) {
-    //         setError(error)
-    //     } finally {
-    //         setIsLoading(false)
-    //     }
-    // }
+    const fetchTodos = async () => {
+        try {
+            setIsLoading(true);
+            const response = await todoWrapper.listTodo('todos/');
+            setTodos(response.data);
+        } catch (error) {
+            setError(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
 
-    // useEffect(()=>{
-    //     fetchTodos()
-    // }, [])
+    useEffect(()=>{
+        fetchTodos()
+    }, [])
 
     return (
         <>
             <h1>Lista de tarefas</h1>
 
-            {/* Se estiver carregando
+            {/* Se estiver carregando */}
             {isLoading && (<p>Carregando tarefas</p>)}
 
             {/* Se tiver erro */}
-            {/* {!isLoading && error && (<p>Erro no cliente</p>)} */}
+            {!isLoading && error && (<p>Erro no cliente</p>)}
 
-            {/* {!isLoading && !error && todos.length > 0 ? (
-                <table></table>
-            ) : (<p>Tarefas não encontradas</p>)} */}
+            {!isLoading && !error && todos.length > 0 ? (
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <td>ID</td>
+                            <td>Tarefa</td>
+                            <td>Usuário</td>
+                            <td>Concluída</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {todos.map((todo)=>(
+                            <tr key={todo.id}>
+                                <td>{todo.id}</td>
+                                <td>{todo.title}</td>
+                                <td>{todo.user}</td>
+                                <td>{todo.is_complete ? 'Sim' : 'Não'}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            ) : (<p>Tarefas não encontradas</p>)}
         </>
     )
 }
